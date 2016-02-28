72925686c521cc7440a0be4a557fa9d283841bd5
Zheng Kai <zhengkai@gmail.com>
2016-02-28 20:05:43 +0800

    up nginx fastcgi_params

--- fpm/install.sh
+++ fpm/install.sh
@@ -6,4 +6,5 @@ sudo touch /etc/default/php-fpm
 sudo cp php-fpm.service /etc/systemd/system/php-fpm.service
 sudo cp php-fpm-checkconf /usr/lib/php/php-fpm-checkconf
 sudo cp php-fpm /etc/init.d/php-fpm
-sudo update-rc.d php-fpm defaults
+
+sudo update-rc.d php-fpm enable
--- nginx/run.sh
+++ nginx/run.sh
@@ -7,3 +7,10 @@ sudo cp nginx.list /etc/apt/sources.list.d/
 sudo apt-key add nginx_signing.key
 sudo apt-get update
 sudo apt-get install -y nginx
+
+if [ -e '/etc/nginx/fastcgi_params' ]; then
+	check_param=`grep 'SCRIPT_FILENAME' /etc/nginx/fastcgi_params`
+	if [ -z "$check_param" ]; then
+		echo -e "\n"'fastcgi_param  SCRIPT_FILENAME    $request_filename;' | sudo tee -a /etc/nginx/fastcgi_params
+	fi
+fi
