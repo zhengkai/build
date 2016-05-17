493cc64c004e21be659efb01a5affb83c36b2339
Zheng Kai <zhengkai@gmail.com>
2016-05-17 11:59:38 +0800

    up

--- fpm/install.sh
+++ fpm/install.sh
@@ -10,4 +10,5 @@ sudo cp php-fpm.service /etc/systemd/system/php-fpm.service
 sudo cp php-fpm-checkconf /usr/lib/php/php-fpm-checkconf
 sudo cp php-fpm /etc/init.d/php-fpm
 
+sudo update-rc.d php-fpm defaults
 sudo update-rc.d php-fpm enable
