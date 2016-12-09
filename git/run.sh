8965a398deddfd96d7f6ffe72c93f14f3b9aab87
Zheng Kai <zhengkai@gmail.com>
2016-12-09 11:42:41 +0800

    up

--- git/run.sh
+++ git/run.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -ex
+#! /bin/bash -e
 
 SRC_DIR='/usr/local/src'
 GIT_SRC_DIR=$SRC_DIR'/git'
@@ -15,15 +15,21 @@ sudo apt-get install -y --no-install-recommends \
 	xmlto
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOCK_FILE=$SCRIPT_DIR'/update.lock'
+VER_FILE=$SCRIPT_DIR'/ver.txt'
+
+if [ ! -e /usr/local/share/info/dir ]; then
+	sudo ln -s /usr/share/info/dir /usr/local/share/info/
+fi
 
 if [ ! -d $SRC_DIR ] || [ ! -w $SRC_DIR ]; then
-	echo 'no dir '$GIT_SRC_DIR
+	>&2 echo 'no dir '$GIT_SRC_DIR
 	exit 1
 fi
 
 cd $SRC_DIR
 if [ "$('pwd')" != $SRC_DIR ]; then
-	echo 'fail to cd '$SRC_DIR
+	>&2 echo 'fail to cd '$SRC_DIR
 	exit 1
 fi
 
@@ -32,30 +38,44 @@ if [ ! -e $GIT_SRC_DIR ]; then
 fi
 cd $GIT_SRC_DIR
 if [ "$('pwd')" != $GIT_SRC_DIR ]; then
-	echo 'fail to cd '$GIT_SRC_DIR
-	exit 1
-fi
-git reset --hard
-git checkout master
-git pull
-
-ver=`git ls-remote --tags | grep -o 'refs/tags/v.*' | grep -v '\^' | grep -v '\[a-z\]+' | cut -d '/' -f 3 | cut -d 'v' -f 2 | grep -v '[a-zA-Z]' | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr -k5,5nr | head -n 1`
-if [ -z "$ver" ]; then
-	echo 'can`t get release tags'
+	>&2 echo 'fail to cd '$GIT_SRC_DIR
 	exit 1
 fi
 
-if ! [ -e /usr/local/share/info/dir ]; then
-	sudo ln -s /usr/share/info/dir /usr/local/share/info/
-fi
+(
+	flock -x -n 200 || exit 1
 
-git checkout 'v'$ver
+	git reset --hard
+	git checkout master
+	git pull
 
-make clean
-make configure
-./configure --prefix=/usr/local
-make all doc info
-sudo make install
-sudo make install-doc
-sudo make install-html
-sudo make install-info
+	PREV_VER=''
+	if [ -f $VER_FILE ]; then
+		PREV_VER=`cat $VER_FILE`
+	fi
+
+	VER=`git ls-remote --tags | grep -o 'refs/tags/v.*' | grep -v '\^' | grep -v '\[a-z\]+' | cut -d '/' -f 3 | cut -d 'v' -f 2 | grep -v '[a-zA-Z]' | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr -k5,5nr | head -n 1`
+	if [ -z "$VER" ]; then
+		>&2 echo 'can`t get release tags'
+		exit 1
+	fi
+
+	if [ "$VER" == "$PREV_VER" ]; then
+		>&2 echo 'newest version '$VER', no need update'
+		exit 1
+	fi
+
+	git checkout 'v'$VER
+
+	make clean 2>&1 || echo
+	make configure
+	./configure --prefix=/usr/local
+	make all doc info
+	sudo make install
+	sudo make install-doc
+	sudo make install-html
+	sudo make install-info
+
+	echo "$VER" > $VER_FILE
+
+) 200>$LOCK_FILE
--- vim/run.sh
+++ vim/run.sh
@@ -5,15 +5,17 @@ SRC_DIR='/usr/local/src'
 VIM_SRC_DIR=$SRC_DIR'/vim'
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOCK_FILE=$SCRIPT_DIR'/update.lock'
+VER_FILE=$SCRIPT_DIR'/ver.txt'
 
 if [ ! -d $SRC_DIR ] || [ ! -w $SRC_DIR ]; then
-	echo 'no dir '$VIM_SRC_DIR
+	>&2 echo 'no dir '$VIM_SRC_DIR
 	exit 1
 fi
 
 cd $SRC_DIR
 if [ "$('pwd')" != $SRC_DIR ]; then
-	echo 'fail to cd '$SRC_DIR
+	>&2 echo 'fail to cd '$SRC_DIR
 	exit 1
 fi
 
@@ -22,16 +24,41 @@ if [ ! -e $VIM_SRC_DIR ]; then
 fi
 cd $VIM_SRC_DIR
 if [ "$('pwd')" != $VIM_SRC_DIR ]; then
-	echo 'fail to cd '$VIM_SRC_DIR
+	>&2 echo 'fail to cd '$VIM_SRC_DIR
 	exit 1
 fi
 
-git pull
+(
+	flock -x -n 200 || exit 1
 
-cd src
+	git reset --hard
+	git checkout master
+	git pull
 
-make clean 2>&1 || echo
+	PREV_VER=''
+	if [ -f $VER_FILE ]; then
+		PREV_VER=`cat $VER_FILE`
+	fi
 
-make
+	VER=`git ls-remote --tags | grep -o 'refs/tags/v.*' | grep -v '\^' | grep -v '\[a-z\]+' | cut -d '/' -f 3 | cut -d 'v' -f 2 | grep -v '[a-zA-Z]' | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr -k5,5nr | head -n 1`
+	if [ -z "$VER" ]; then
+		>&2 echo 'can`t get release tags'
+		exit 1
+	fi
 
-sudo make install
+	if [ "$VER" == "$PREV_VER" ]; then
+		>&2 echo 'newest version '$VER', no need update'
+		exit 1
+	fi
+
+	git checkout 'v'$VER
+
+	cd src
+
+	make clean 2>&1 || echo
+	make
+	sudo make install
+
+	echo "$VER" > $VER_FILE
+
+) 200>$LOCK_FILE
