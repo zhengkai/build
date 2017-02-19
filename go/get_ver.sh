c720b09ef092fcfc8735afa56b947f771ab70c81
Zheng Kai <zhengkai@gmail.com>
2017-02-20 00:57:09 +0800

    fix go两位版本号

--- go/get_ver.sh
+++ go/get_ver.sh
@@ -6,7 +6,7 @@
 
 cd $(dirname `readlink -f $0`)
 
-VER=`curl https://golang.org/dl/ 2>/dev/null | grep -o -P '<a class="download downloadBox" href="https://storage\.googleapis\.com/golang/go\d+\.\d+\.\d+\.linux-amd64\.tar\.gz">' | head -n 1 | grep -o -P "\d+\.\d+\.\d+"`
+VER=`curl https://golang.org/dl/ 2>/dev/null | grep -o -P '<a class="download downloadBox" href="https://storage\.googleapis\.com/golang/go\d+\.\d+(\.\d+)?\.linux-amd64\.tar\.gz">' | head -n 1 | grep -o -P "\d+\.\d+(\.\d+)?"`
 
 if [ -z "$VER" ]; then
 	>&2 echo 'can not detect go version'
