d92f221e8289066877ff2f2aeffb7ff228fbb7cb
Zheng Kai <zhengkai@gmail.com>
2018-02-23 15:12:31 +0800

    up

--- go/bin.sh
+++ go/bin.sh
@@ -11,7 +11,7 @@ fi
 GOBIN_NAME='go'$GOBIN_VER'.linux-amd64.tar.gz'
 GOBIN_FILE='/tmp/'$GOBIN_NAME
 if ! [ -e $GOBIN_FILE ]; then
-	wget 'https://storage.googleapis.com/golang/'$GOBIN_NAME -O $GOBIN_FILE
+	wget 'https://dl.google.com/go/'$GOBIN_NAME -O $GOBIN_FILE
 fi
 
 if [ -e /usr/local/go ]; then
