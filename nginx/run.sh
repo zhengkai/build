8eeed86358726dcc29290658f27d1a2eb72b99cb
Zheng Kai <zhengkai@gmail.com>
2018-08-20 16:19:54 +0800

    nginx key

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,7 +3,7 @@
 cd $(dirname `readlink -f $0`)
 
 sudo cp nginx.list /etc/apt/sources.list.d/
-sudo apt-key add nginx_signing.key
+sudo apt-key add nginx-signing.key
 sudo apt-get update
 sudo apt-get install -y nginx
 
