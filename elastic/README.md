159ec3b97942352bf66873698278843773b9022e
Zheng Kai <zhengkai@gmail.com>
2018-09-20 16:00:38 +0800

    up

--- elastic/README.md
+++ elastic/README.md
@@ -1,1 +1,3 @@
 https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
+
+https://www.elastic.co/guide/en/kibana/current/deb.html
--- elastic/run.sh
+++ elastic/run.sh
@@ -8,9 +8,12 @@ sudo cp source.list /etc/apt/sources.list.d/elasticsearch.list
 sudo apt-key add gpg.key
 sudo apt update
 
-sudo apt install elasticsearch
+sudo apt install elasticsearch kibana logstash
 
 sudo systemctl daemon-reload
-sudo systemctl enable elasticsearch.service
 
+sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
+
+sudo systemctl enable kibana.service
+sudo systemctl start kibana.service
