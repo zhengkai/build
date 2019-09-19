001f87a8cd3ab778bd1a14d9523924f306201933
Zheng Kai <zhengkai@gmail.com>
2019-09-19 15:44:12 +0800

    node

--- nodejs/stable.sh
+++ nodejs/stable.sh
@@ -9,19 +9,31 @@ INSTALL_DIR='/usr/local/'
 SRC_DIR='/usr/local/src/'
 
 SCRIPT_DIR="$( cd "`dirname "$0"`" && pwd )"
-LOCK_FILE=$SCRIPT_DIR'/update.lock'
-VER_FILE=$SCRIPT_DIR'/ver.txt'
+LOCK_FILE="${SCRIPT_DIR}/update.lock"
+VER_FILE="${SCRIPT_DIR}/ver.txt"
 
-VER=`curl -s https://nodejs.org/en/ | grep -Po 'Download \d+\.\d+(\.\d+)? '$VER_CHOOSE | cut -d ' ' -f 2`
+VER=`curl -s https://nodejs.org/en/ | grep -Po "Download \d+\.\d+(\.\d+)? ${VER_CHOOSE}" | cut -d ' ' -f 2`
 
 if [ -z "$VER" ]; then
 	>&2 echo 'cannot detect version'
 	exit 1
 fi
 
-FILE='node-v'$VER'-linux-x64.tar.xz'
-FILE_DIR=$SRC_DIR'node-v'$VER
-URL='https://nodejs.org/dist/v'$VER'/'$FILE
+ARCH=`arch`
+if [ "$ARCH" == 'x86_64' ]; then
+	ARCH='x64'
+elif [ "$ARCH" == 'aarch64' ]; then
+	ARCH='arm64'
+else
+	>&2 echo unknown arch $ARCH
+	exit
+fi
+
+FILE="node-v${VER}-linux-${ARCH}.tar.xz"
+FILE_DIR="${SRC_DIR}node-v${VER}"
+URL="https://nodejs.org/dist/v${VER}/${FILE}"
+
+echo $URL
 
 (
 	flock -x -n 200 || exit 1
@@ -36,19 +48,19 @@ URL='https://nodejs.org/dist/v'$VER'/'$FILE
 		exit 1
 	fi
 
-	wget $URL -O $SRC_DIR$FILE
-	mkdir -p $FILE_DIR
-	tar -xf $SRC_DIR$FILE -C $FILE_DIR --strip-components=1
+	wget "$URL" -O "${SRC_DIR}${FILE}"
+	mkdir -p "$FILE_DIR"
+	tar -xf "${SRC_DIR}${FILE}" -C "$FILE_DIR" --strip-components=1
 
 	SUB_DIR=('bin' 'include' 'lib' 'share')
 	for i in "${SUB_DIR[@]}"; do
-		sudo cp -r $FILE_DIR'/'$i $INSTALL_DIR
+		sudo cp -R "${FILE_DIR}/${i}" "$INSTALL_DIR"
 	done
 
-	if [ ! -e $INSTALL_DIR'bin/nodejs' ]; then
-		sudo ln -s $INSTALL_DIR'bin/node' $INSTALL_DIR'bin/nodejs'
+	if [ ! -e "${INSTALL_DIR}bin/nodejs" ]; then
+		sudo ln -s "${INSTALL_DIR}bin/node" "${INSTALL_DIR}bin/nodejs"
 	fi
 
-	echo "$VER" > $VER_FILE
+	echo "$VER" > "$VER_FILE"
 
-) 200>$LOCK_FILE
+) 200>"$LOCK_FILE"
