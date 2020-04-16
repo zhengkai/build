18d7f3a6201f2baa9b7a44e9d82de64f9358645f
Zheng Kai <zhengkai@gmail.com>
2020-04-16 11:38:25 +0800

    protoc

---
+++ protoc/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+
+VER="3.11.4"
+
+ARCH=$(arch)
+if [ "$ARCH" == 'x86_64' ]; then
+	ARCH='x86_64'
+elif [ "$ARCH" == 'aarch64' ]; then
+	ARCH='aarch_64'
+else
+	>&2 echo unknown arch $ARCH
+	exit 1
+fi
+
+ZIP="protoc-${VER}-linux-${ARCH}.zip"
+
+LOCAL_ZIP="/usr/local/src/$ZIP"
+
+curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${VER}/${ZIP}" -o "$LOCAL_ZIP"
+
+MIN_SIZE=1000000
+SIZE=$(wc -c <"$LOCAL_ZIP")
+if [ "$SIZE" -lt $MIN_SIZE ]; then
+	>&2 echo file downloaded, but too small
+	>&2 echo "$LOCAL_ZIP ($SIZE)"
+	exit 1
+fi
+
+sudo unzip -o "$LOCAL_ZIP" -d /usr/local bin/protoc
+sudo unzip -o "$LOCAL_ZIP" -d /usr/local 'include/*'
