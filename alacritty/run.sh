d5652570eeb9ac04f4c26d38de480ac0d0505960
Zheng Kai <zhengkai@gmail.com>
2020-05-29 23:03:30 +0800

    up

--- alacritty/run.sh
+++ alacritty/run.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 sudo apt-get install cargo cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
 
-git clone git@github.com:alacritty/alacritty.git /usr/local/src/alacritty
+DIR="/usr/local/src/alacritty"
+
+git clone git@github.com:alacritty/alacritty.git "$DIR"
+
+cd "$DIR"
+
+cargo build --release
+
+sudo cp target/release/alacritty /usr/local/bin/
+
+sudo cp extra/completions/_alacritty /usr/share/zsh/vendor-completions/
