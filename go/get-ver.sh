a44ca25cb3ca490fe7a0f23c531f1a8b61b56b4c
Zheng Kai <zhengkai@gmail.com>
2020-08-12 10:29:57 +0800

    golang

--- go/get-ver.sh
+++ go/get-ver.sh
@@ -4,13 +4,13 @@
 #
 # https://golang.org/dl/
 
-cd $(dirname `readlink -f $0`)
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
 
-VER=`curl -s https://golang.org/dl/ 2>/dev/null | grep -o -P '<a class="download downloadBox" href="https://(storage\.googleapis\.com/golang|redirector\.gvt1\.com/edgedl|dl\.google\.com)/go/go\d+\.\d+(\.\d+)?\.linux-amd64\.tar\.gz">' | head -n 1 | grep -o -P "\d+\.\d+(\.\d+)?"`
+VER=$(curl -s https://golang.org/dl/ 2>/dev/null | grep -o -P '<a class="download downloadBox" href="/dl/go\d+\.\d+(\.\d+)?\.linux-amd64\.tar\.gz">' | head -n 1 | grep -o -P "\d+\.\d+(\.\d+)?")
 
 if [ -z "$VER" ]; then
 	>&2 echo 'can not detect go version'
 	exit 1
 fi
 
-echo $VER
+echo "$VER"
