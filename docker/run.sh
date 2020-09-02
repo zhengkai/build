ff26b448d0ff129567d1aecd001af49fdb933ecf
Zheng Kai <zhengkai@gmail.com>
2020-09-02 14:32:14 +0800

    docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+# https://docs.docker.com/engine/install/ubuntu/
 
 CODENAME=$(lsb_release -c -s)
 
@@ -16,18 +16,19 @@ fi
 
 DIR=$(readlink -f "$0") && DIR=$(dirname "$DIR") && cd "$DIR" || exit 1
 
+sudo apt-key add gpg.key
+
 SOURCE='source.list'
 
 echo "deb [arch=${ARCH}] https://download.docker.com/linux/ubuntu ${CODENAME} stable" > "$SOURCE"
 cat "$SOURCE"
 sudo cp "$SOURCE" /etc/apt/sources.list.d/docker.list
 
+sudo apt update
 sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-sudo apt-key add gpg.key
-sudo apt update
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce docker-ce docker-ce-cli containerd.io
