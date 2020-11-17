f48f798f75dc101f2846316c8771e7bb1d27c61c
Zheng Kai <zhengkai@gmail.com>
2020-11-17 18:51:55 +0800

    kitty

---
+++ kitty/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+sudo apt install -y libdbus-1-dev libxcursor-dev libxrandr-dev libxi-dev libxinerama-dev libgl1-mesa-dev libxkbcommon-x11-dev libfontconfig-dev \
+	libharfbuzz-dev liblcms2-dev libx11-xcb-dev libwayland-dev wayland-protocols
+
+cd /usr/local/src
+
+if [ ! -d kitty ]; then
+	git clone https://github.com/kovidgoyal/kitty.git
+fi
+
+cd kitty
+
+git pull --rebase
+
+make clean || :
+make
+
+sudo cp ./kitty/launcher/kitty /usr/local/bin
+sudo cp terminfo/x/xterm-kitty /usr/share/terminfo/x/
