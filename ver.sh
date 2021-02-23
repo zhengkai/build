17738172a42aeab266d1b7a010da46bb547e1d07
Zheng Kai <zhengkai@gmail.com>
2021-02-23 10:16:59 +0800

    up

--- ver.sh
+++ ver.sh
@@ -5,8 +5,8 @@ DIR_LIST=('git' 'go' 'nodejs' 'shadowsocks' 'vim' 'watchman')
 for DIR in "${DIR_LIST[@]}"; do
 	STATUS='not installed'
 	FILE=$DIR'/ver.txt'
-	if [ -s $FILE ]; then
-		STATUS=`cat $FILE`
+	if [ -s "$FILE" ]; then
+		STATUS=$(cat "$FILE" || :)
 	fi
 	printf '%20s  %s\n' "$DIR" "$STATUS"
 	echo
