34da676bc7a8811c7c6a8cc4c2282d5dc2583f9b
Zheng Kai <zhengkai@gmail.com>
2024-10-24 10:03:00 +0800

    up

--- restore-time/run.sh
+++ restore-time/run.sh
@@ -2,15 +2,22 @@
 
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
 
-BASH_FILE="/usr/local/bin/restore-time"
+NAME="restore-time"
+
+BASH_FILE="/usr/local/bin/${NAME}"
 if [ ! -e "$BASH_FILE" ]; then
 	sudo cp ./restore-time "$BASH_FILE"
 fi
 
-SERVICE="restore-time.service"
+SERVICE="${NAME}.service"
 SERVICE_FILE="/etc/systemd/system/${SERVICE}"
 if [ ! -e "$SERVICE_FILE" ]; then
 	sudo cp ./service "$SERVICE_FILE"
+fi
+
+CRON="/etc/cron.d/${NAME}"
+if [ ! -e "$CRON" ]; then
+	sudo cp ./crontab "$CRON"
 fi
 
 sudo systemctl enable "$SERVICE"
