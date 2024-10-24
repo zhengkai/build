98b6e819c9d3249c3104193c1dc6e299f0f7686c
Zheng Kai <zhengkai@gmail.com>
2024-10-24 09:42:47 +0800

    up

--- daily.sh
+++ daily.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd $(dirname `readlink -f $0`)
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
 
 ./git/run.sh
 ./go/update_bin.sh
 ./nodejs/stable.sh
 ./nodejs/npm.sh
-./shadowsocks/run.sh
-./vim/run.sh
-./watchman/run.sh
+#./shadowsocks/run.sh
+#./vim/run.sh
+#./watchman/run.sh
