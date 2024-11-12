fee50cb1ddc78685bddce32bbccc4dbaf718d03c
Zheng Kai <zhengkai@gmail.com>
2024-11-12 12:45:32 +0800

    up

--- ss-rust/get-ver.sh
+++ ss-rust/get-ver.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-RELEASE_FILE="ver/releases.json"
+PACKAGE=${1:-shadowsocks/shadowsocks-rust}
 
-cd "$(dirname "$(readlink -f "$0")")" || exit 1
-
-LOCKFILE="./update-ver.lock"
-
-(
-
-flock -n 200 || exit 1
-
-mkdir -p ver
-
-if [ -e "$RELEASE_FILE" ]; then
-	rm "$RELEASE_FILE"
-fi
-wget 'https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases' -O "$RELEASE_FILE"
-
-jq -r '.[] | .name' "$RELEASE_FILE" |  sort -Vr | head -n 1 | tee ver/current.txt
-
-) 200>${LOCKFILE}
+curl "https://api.github.com/repos/${PACKAGE}/releases" 2>/dev/null \
+	| jq -r '.[] | .name' - \
+	| sort -Vr \
+	| head -n 1
--- ss-rust/qtun.sh
+++ ss-rust/qtun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PACKAGE="shadowsocks/shadowsocks-rust"
+PACKAGE="shadowsocks/qtun"
 
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
 
-VER=$(./get-ver.sh "$PACKAGE") || VER="v1.21.2"
+VER=$(./get-ver.sh "$PACKAGE") || VER="v0.2.0"
 
 echo "$VER"
 
@@ -15,17 +15,17 @@ if [ ! -w "$DST_BASE" ]; then
 	exit
 fi
 
-FILE="shadowsocks-${VER}.$(arch)-unknown-linux-musl.tar.xz"
+FILE="qtun-${VER}.$(arch)-unknown-linux-musl.tar.xz"
 URL="https://github.com/${PACKAGE}/releases/download/${VER}/${FILE}"
 
 wget "$URL" -O "${DST_BASE}/${FILE}" || exit 1
 
-DST="${DST_BASE}/ss-rust-${VER}"
+DST="${DST_BASE}/qtun-${VER}"
 mkdir -p "$DST"
 
 tar -xvf "${DST_BASE}/${FILE}" -C "$DST"
 
-FILE_LIST=("sslocal" "ssmanager" "ssserver" "ssservice" "ssurl")
+FILE_LIST=("qtun-client" "qtun-server")
 for FILE in "${FILE_LIST[@]}"; do
 	sudo cp "${DST}/${FILE}" /usr/local/bin/
 done
