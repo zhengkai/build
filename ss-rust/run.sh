c0494de217ec8139a8a2656a9608e7a4365c0219
Zheng Kai <zhengkai@gmail.com>
2024-11-12 10:28:56 +0800

    up

--- ss-rust/run.sh
+++ ss-rust/run.sh
@@ -7,23 +7,13 @@ VER=${VER:-v1.21.2}
 
 echo "$VER"
 
-ARCH=$(arch)
-if [ "$ARCH" == 'x86_64' ]; then
-	ARCH='amd64'
-elif [ "$ARCH" == 'aarch64' ]; then
-	ARCH='arm64'
-else
-	>&2 echo "unknown arch $ARCH"
-	exit
-fi
-
 DST_BASE="/usr/local/src"
 if [ ! -w "$DST_BASE" ]; then
 	>&2 echo "no write permission to $DST_BASE"
 	exit
 fi
 
-FILE="shadowsocks-${VER}.x86_64-unknown-linux-musl.tar.xz"
+FILE="shadowsocks-${VER}.$(arch)-unknown-linux-musl.tar.xz"
 URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VER}/${FILE}"
 
 wget "$URL" -O "${DST_BASE}/${FILE}" || exit 1
@@ -34,3 +24,5 @@ mkdir -p "$DST"
 tar -xvf "${DST_BASE}/${FILE}" -C "$DST"
 
 sudo cp "${DST}"/ss* /usr/local/bin/
+
+hash -r
