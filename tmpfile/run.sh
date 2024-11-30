7179261deedd487145110d0c1cc84328fd148c55
Zheng Kai <zhengkai@gmail.com>
2024-11-30 10:58:34 +0800

    up

--- tmpfile/run.sh
+++ tmpfile/run.sh
@@ -3,3 +3,7 @@
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
 
 sudo cp zhengkai.conf /etc/tmpfiles.d/
+
+if ! sudo grep -q "DefaultRuntimeDirectory" "/etc/systemd/system.conf"; then
+	sudo sed -i '/^\[Manager\]$/a DefaultRuntimeDirectory=/tmp/systemd' /etc/systemd/system.conf
+fi
