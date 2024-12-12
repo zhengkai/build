a4e6647c850a3623f12f609f4ecdc6ebde5fc9ad
Zheng Kai <zhengkai@gmail.com>
2024-12-12 22:40:58 +0800

    gfm

---
+++ gfm/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+cd /usr/local/src
+
+if [ ! -d cmark-gfm ]; then
+	git clone https://github.com/github/cmark-gfm.git
+fi
+cd cmark-gfm
+git pull
+
+mkdir -p build
+cd build
+cmake ..
+make clean
+make
+make test
+sudo make install
+sudo ldconfig
