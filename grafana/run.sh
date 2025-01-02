72d757af2dbdd1f9f1ea141093dfe604081d1789
Zheng Kai <zhengkai@gmail.com>
2025-01-02 15:47:48 +0800

    up

--- grafana/run.sh
+++ grafana/run.sh
@@ -4,11 +4,10 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 
 # https://packages.grafana.com/oss/
-wget -qO - https://apt.grafana.com/gpg.key \
-	| sudo gpg --dearmor -o /etc/apt/keyrings/grafana.gpg >/dev/null
+sudo wget -q https://apt.grafana.com/gpg.key -O /etc/apt/keyrings/grafana.key
 
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" \
-	| sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [arch=arm64,signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" \
+	| sudo tee /etc/apt/sources.list.d/grafana.list
 
 sudo apt-get update
 sudo apt-get install grafana
--- prometheus/pull.sh
+++ prometheus/pull.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -ex
 
-sudo docker pull prom/prometheus-linux-amd64:latest
+ARCH=$(arch)
+if [ "$ARCH" == 'x86_64' ]; then
+    ARCH='amd64'
+elif [ "$ARCH" == 'aarch64' ]; then
+    ARCH='armv7'
+else
+    >&2 echo "unknown arch $ARCH"
+    exit
+fi
+
+sudo docker pull "prom/prometheus-linux-${ARCH}:latest"
