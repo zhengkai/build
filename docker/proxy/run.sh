340049b6fa591488191769172c1bdf7e707ced4a
Zheng Kai <zhengkai@gmail.com>
2025-08-18 17:24:13 +0800

    docker

---
+++ docker/proxy/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export PROXY="127.0.0.1:8118"
+export NOPROXY="*.tuna.tsinghua.edu.cn,*.aliyuncs.com,10.0.0.0/8,127.0.0.0/8"
+
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+
+DAEMON_JSON="/etc/docker/daemon.json"
+echo "$DAEMON_JSON"
+echo
+if [ -e "$DAEMON_JSON" ]; then
+	envsubst < ./daemon.json
+else
+	envsubst < ./daemon.json | sudo tee "$DAEMON_JSON"
+fi
+echo
+
+SERVICE_CONF="/etc/systemd/system/docker.service.d/http-proxy.conf"
+echo "$SERVICE_CONF"
+echo
+if [ -e "$SERVICE_CONF" ]; then
+	envsubst < ./service.conf
+else
+	sudo mkdir -p "$(dirname "$SERVICE_CONF")"
+	envsubst < ./service.conf | sudo tee "$SERVICE_CONF"
+fi
+echo
+
+ROOT_JSON="/root/.docker/config.json"
+echo "$ROOT_JSON"
+echo
+if [ -e "$ROOT_JSON" ]; then
+	envsubst < ./config.json
+else
+	sudo mkdir -p "$(dirname "$SERVICE_CONF")"
+	envsubst < ./config.json | sudo tee "$ROOT_JSON"
+fi
+echo
+
+CONTAINERD_TOML="/etc/containerd/config.toml"
+echo "$CONTAINERD_TOML"
+echo
+envsubst < ./containerd.toml
+echo
+
+#sudo systemctl daemon-reload
+#sudo systemctl restart docker
