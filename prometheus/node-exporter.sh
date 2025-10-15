4d68b54428dd86cbfd8ec09bef50953398ebebfd
Zheng Kai <zhengkai@gmail.com>
2025-10-15 14:57:32 +0800

    node-exporter

---
+++ prometheus/node-exporter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo docker run -d \
+  --name=node-exporter \
+  --restart=always \
+  --net="host" \
+  --pid="host" \
+  -v "/:/host:ro,rslave" \
+  quay.io/prometheus/node-exporter:latest \
+  --path.rootfs=/host
