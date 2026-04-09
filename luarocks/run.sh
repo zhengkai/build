ac933f039b6491aa04bfecd2ea3182d685a1cd61
Zheng Kai <zhengkai@gmail.com>
2026-04-09 12:29:42 +0800

    up

---
+++ luarocks/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /usr/local/src || exit 1
+
+wget https://luarocks.org/releases/luarocks-3.13.0.tar.gz
+tar zxpf luarocks-3.13.0.tar.gz
+cd luarocks-3.13.0 || exit 1
+./configure && make && sudo make install
